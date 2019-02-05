f93bbcbce7f8eda497d6794f954b280f8ff043fa
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-02-05 19:54:53 +0100

    Added onclick method

--- app/src/main/java/com/example/potatohead/MainActivity.java
+++ app/src/main/java/com/example/potatohead/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.potatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkClicked(View v) {
+        Log.d("potato", "checkClicked: ");
+        CheckBox checkbox = (CheckBox) v;
+        //String check = checkbox.getText().toString();
+        int number = checkbox.getId();
+        ImageView image = (ImageView) findViewById(number);
+        if(image.getVisibility() == View.INVISIBLE){
+            image.setVisibility(View.VISIBLE);
+        }
+        else {
+            image.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
