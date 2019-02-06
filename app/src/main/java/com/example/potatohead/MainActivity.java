098d5b34d069735bf7d3e02192c6345dfa89c2fe
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-02-06 19:18:50 +0100

    Added find ID method

--- app/src/main/java/com/example/potatohead/MainActivity.java
+++ app/src/main/java/com/example/potatohead/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int KOLOM = 0;
+    private int [][] arr = {{R.id.arms, R.id.checkArms}, {R.id.mustache, R.id.checkMustache}, {R.id.ears, R.id.checkEars},
+            {R.id.mouth, R.id.checkMouth}, {R.id.eyebrows, R.id.checkEyebrows}, {R.id.shoes, R.id.checkShoes},
+            {R.id.glasses, R.id.checkGlasses}, {R.id.nose, R.id.checkNose}, {R.id.hat, R.id.checkHat},
+            {R.id.eyes, R.id.checkEyes}};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +24,29 @@ public class MainActivity extends AppCompatActivity {
     public void checkClicked(View v) {
         Log.d("potato", "checkClicked: ");
         CheckBox checkbox = (CheckBox) v;
-        //String check = checkbox.getText().toString();
-        int number = checkbox.getId();
-        ImageView image = (ImageView) findViewById(number);
+        int check = checkbox.getId();
+        ImageView image = (ImageView) findViewById(arr[findID(check)][KOLOM]);
         if(image.getVisibility() == View.INVISIBLE){
             image.setVisibility(View.VISIBLE);
         }
         else {
             image.setVisibility(View.INVISIBLE);
         }
+    }
+
+    //find the image ID linked to the clicked checkbox
+    public int findID(int id)
+    {
+        int row = 0;
+        for (int i =0; i < arr.length ; i++ ){
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == id) {
+                    row = i;
+                    break;
+                }
+            }
+        }
+        return row;
     }
 
 }
